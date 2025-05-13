54e7adc4ebb2b8c7acd78921a68127215168192c
AlfonsoRG0720 <alfonsor814@gmail.com>
2025-05-13 21:47:27 +0200

    Actualización BBDD
    Se agregan álbumes de música a la BBDD

--- scripts/BBDD.js
+++ scripts/BBDD.js
@@ -194,6 +194,223 @@ export let listaDiscos= [
         anio:2022,
         imagen:"29.jpg",
         precio:13.50
+    },
+    {
+        id:30,
+        nombre:"Legado de Juglares",
+        anio:2004,
+        imagen:"30.jpg",
+        precio:16
+    },
+    {
+        id:31,
+        nombre:"JuglarMetal",
+        anio:2006,
+        imagen:"31.jpg",
+        precio:14.50
+    },
+    {
+        id:32,
+        nombre:"Once Romances - Desde al-Andalus",
+        anio:2008,
+        imagen:"32.jpg",
+        precio:12.50
+    },
+    {
+        id:33,
+        nombre:"Sueños",
+        anio:2015,
+        imagen:"33.jpg",
+        precio:16.00
+    },
+    {
+        id:34,
+        nombre:"La Magia de la Luna",
+        anio:2017,
+        imagen:"34.jpg",
+        precio:17.50
+    },
+    {
+        id:35,
+        nombre:"Amantes Sunt Amentes",
+        anio:2006,
+        imagen:"35.jpg",
+        precio:18.00
+    },
+    {
+        id:36,
+        nombre:"Para Ti Con Desprecio",
+        anio:2005,
+        imagen:"36.jpg",
+        precio:16.50
+    },
+    {
+        id:37,
+        nombre:"Sangre fría",
+        anio:2007,
+        imagen:"37.jpg",
+        precio:18.50
+    },
+    {
+        id:38,
+        nombre:"Todxs Somos Pxndx",
+        anio:2020,
+        imagen:"38.jpg",
+        precio:20.00
+    },
+    {
+        id:39,
+        nombre:"Caramelos De Cianuro",
+        anio:2010,
+        imagen:"39.jpg",
+        precio:18.50
+    },
+    {
+        id:40,
+        nombre:"Frisbee",
+        anio:2002,
+        imagen:"40.jpg",
+        precio:17.50
+    },
+    {
+        id:41,
+        nombre:"Miss Mujerzuela",
+        anio:2000,
+        imagen:"41.jpg",
+        precio:14.00
+    },
+    {
+        id:42,
+        nombre:"En Vivo 2008 (En Vivo)",
+        anio:2017,
+        imagen:"42.jpg",
+        precio:16.00
+    },
+    {
+        id:43,
+        nombre:"Mi Sangre",
+        anio:2004,
+        imagen:"43.jpg",
+        precio:20.00
+    },
+    {
+        id:44,
+        nombre:"Un Día Normal",
+        anio:2002,
+        imagen:"44.jpg",
+        precio:18.00
+    },
+    {
+        id:45,
+        nombre:"La Vida Es Un Ratico",
+        anio:2007,
+        imagen:"45.jpg",
+        precio:19.00
+    },
+    {
+        id:46,
+        nombre:"Folktergeist",
+        anio:2002,
+        imagen:"46.jpg",
+        precio:16.50
+    },
+    {
+        id:47,
+        nombre:"Love and Oz",
+        anio:2011,
+        imagen:"47.jpg",
+        precio:14.00
+    },
+    {
+        id:48,
+        nombre:"Rarezas",
+        anio:2006,
+        imagen:"48.jpg",
+        precio:14.50
+    },
+    {
+        id:49,
+        nombre:"La Bruja",
+        anio:1997,
+        imagen:"49.jpg",
+        precio:21.00
+    },
+    {
+        id:50,
+        nombre:"Gaia Epilogo",
+        anio:2009,
+        imagen:"50.jpg",
+        precio:19.00
+    },
+    {
+        id:51,
+        nombre:"Diabulus In Musica",
+        anio:2006,
+        imagen:"51.jpg",
+        precio:18.50
+    },
+    {
+        id:52,
+        nombre:"La Flaca",
+        anio:1996,
+        imagen:"52.jpg",
+        precio:16.50
+    },
+    {
+        id:53,
+        nombre:"Bonito",
+        anio:2003,
+        imagen:"53.jpg",
+        precio:14.00
+    },
+    {
+        id:54,
+        nombre:"Recuérdame Con Cartas y Algo Más... Los Éxitos",
+        anio:2012,
+        imagen:"54.jpg",
+        precio:14.50
+    },
+    {
+        id:55,
+        nombre:"El Mundo Se Equivoca",
+        anio:2006,
+        imagen:"55.jpg",
+        precio:19.50
+    },
+    {
+        id:56,
+        nombre:"Lo Que Te Conte Mientras Te Hacias La Dormida",
+        anio:2000,
+        imagen:"56.jpg",
+        precio:17.50
+    },
+    {
+        id:57,
+        nombre:"LOVG: Grandes Éxitos",
+        anio:2008,
+        imagen:"57.jpg",
+        precio:16.00
+    },
+    {
+        id:58,
+        nombre:"Puedes Contar Conmigo",
+        anio:2003,
+        imagen:"58.jpg",
+        precio:20.00
+    },
+    {
+        id:59,
+        nombre:"El Silencio",
+        anio:1992,
+        imagen:"59.jpg",
+        precio:17.50
+    },
+    {
+        id:60,
+        nombre:"Me Verás Volver (Hits & Más)",
+        anio:2007,
+        imagen:"60.jpg",
+        precio:18.00
     }
 ]
 
